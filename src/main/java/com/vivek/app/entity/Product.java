90f0c9d184808b527d706f533442445001ab8029
Vivek Mishra <vivekmishra22117@gmail.com>
2018-03-06 22:35:50 +0530

    MVC2001_Added_Model_package

--- src/main/java/com/vivek/app/entity/Product.java
+++ src/main/java/com/vivek/app/entity/Product.java
@@ -1,9 +1,16 @@
 package com.vivek.app.entity;
 
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 @Table(name="Products")
@@ -11,6 +18,77 @@ public class Product implements Serializable {
 
 
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@Column(name="Code", nullable=false, length=20)
+	private String code;
+	
+	@Column(name="Product_Name",nullable=false,length=255)
+	private String name;
+	
+	@Column(name="P_Price",nullable=false)
+	private double price;
+	
+	@Lob
+	@Column(name="Image",length=Integer.MAX_VALUE,nullable=true)
+	private byte[] image;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name="Create_Date",nullable=false)
+	private Date createDate;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public byte[] getImage() {
+		return image;
+	}
+
+	public void setImage(byte[] image) {
+		this.image = image;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [code=" + code + ", name=" + name + ", price=" + price + ", image=" + Arrays.toString(image)
+				+ ", createDate=" + createDate + "]";
+	}
+	
+	
+	
 	
 	
 
---
+++ src/main/java/com/vivek/app/model/CustomerInfo.java
@@ -0,0 +1,54 @@
+package com.vivek.app.model;
+
+public class CustomerInfo {
+	
+	private String name;
+	private String address;
+	private String phone;
+	private String email;
+	
+	private boolean isValid;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public boolean isValid() {
+		return isValid;
+	}
+
+	public void setValid(boolean isValid) {
+		this.isValid = isValid;
+	}
+	
+	
+
+}
